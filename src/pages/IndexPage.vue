9892b4d3e1731e545a22fa4d5b61e979fed94bb7
RedmanCL <withneywayne@gmail.com>
2024-07-09 03:39:14 +0100

    added hero section, and body contents

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,538 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="">
+    <!-- Header Section-->
+    <div class="header-parent">
+      <img src="../../public/bg.jpg" alt="">
+      <div class="hero-section-logos-contianer">
+        <img src="../../public/gsl.png" alt="">
+        <img src="../../public/galH.png" alt="">
+        <img src="../../public/Medea.png" alt="">
+      </div>
+      <div class="header-container">
+        <div class="grey-logo">
+          <img src="../../public/gsl.png" alt="Logo">
+        </div>
+        <nav class="header-nav">
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#fees">Tuition Fees</a></li>
+            <li><a href="#learning">Learning Approach</a></li>
+            <li><a href="#documents">Documents</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- Hero Section -->
+      <div class="hero-section-main-contianer">
+        <div class="hero-section-contianer">
+          <div class="hero-section-text-contianer">
+            <h1>Join Our Civil Engineering Bachelor’s Degree Program</h1>
+            <p>Partnered with MEDEA International University and Galilee International Management Institute</p>
+          </div>
+          <div class="button-container">
+            <a href="#" class="enroll-button hero-enroll-button">ENROLL NOW <img src="../../public/arrow.png"
+                alt=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Main content Section -->
+    <div class="main-section-container">
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <div class="content">
+            <h2>Civil Engineering Bachelor’s Degree with MEDEA International University</h2>
+            <p>Greysoft Technologies has partnered with Galilee International Management Institute to offer a Civil
+              Engineering Bachelor’s Degree to Nigerian students at MEDEA International University.</p>
+          </div>
+
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+          <div class="content">
+            <h2>Job Placements</h2>
+            <p>Graduates have the chance to secure up to three-year job placements in civil engineering firms in North
+              America and Northern Europe, offering valuable hands-on experience. These positions typically come with
+              contracts that include monthly salaries ranging from €2,000 to €3,000, setting you on a solid path
+              towards a successful career in civil engineering.</p>
+          </div>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <div class="content">
+            <h2>About MEDEA International University</h2>
+            <p>MED.E.A Higher Education Institution for Mediterranean, Europe and Africa, previously known as Pegaso
+              International, is a member of the Multiversity Group. It is renowned for providing a flexible,
+              inclusive, and accessible education to international students, offering innovative online multilingual
+              programmes designed to enhance job opportunities worldwide.</p>
+          </div>
+
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+          <div class="content">
+            <h2>Tuition Fees</h2>
+            <p>The total tuition fee for the 3-year duration of the programme is 6,000 Euros.</p>
+            <ul>
+              <li>1st Year: 3,000 Euros</li>
+              <li>2nd Year: 1,500 Euros</li>
+              <li>3rd Year: 1,500 Euros</li>
+            </ul>
+            <p>Early Birds who enroll on or before the end of June 2024 will save 1,000 Euros. Installments are allowed.
+            </p>
+            <a href="#" class="enroll-button">ENROLL NOW</a>
+          </div>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <div class="content">
+            <h2>Learning Approach</h2>
+            <p>The Bachelor’s Degree in Civil Engineering is delivered entirely online, providing a flexible learning
+              environment for students. The programme uses advanced e-learning methods and platforms to facilitate
+              coursework, lectures, and interaction with professors and peers. This online approach ensures a
+              comprehensive education while benefiting from the flexibility of distance learning.</p>
+            <a href="#" class="enroll-button">ENROLL NOW</a>
+          </div>
+
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+          <div class="content">
+            <h2>Required Documents</h2>
+            <p>To register for the programme, you need to provide the following documents:</p>
+            <ul>
+              <li>A copy of your valid ID card or passport.</li>
+              <li>Your High School Diploma Certificate in English.</li>
+              <li>A letter from your graduated High School confirming completion of at least 12 years of schooling and
+                qualification for university studies (Level 5 according to EQF or equivalent).</li>
+              <li>An updated CV.</li>
+              <li>English language certification at a minimum level of B2.</li>
+            </ul>
+            <a href="#" class="enroll-button">ENROLL NOW</a>
+          </div>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card" flat bordered>
+        <q-card-section horizontal>
+          <div class="content">
+            <h2>Class Starts</h2>
+            <p>The next academic year is 2024/2025. Your study period starts on 1 August 2024 until 31 July 2025. You
+              can start at any time during the academic year.</p>
+            <p>Ensure to contact Greysoft Technologies Nigeria before enrolling.</p>
+          </div>
+
+          <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        </q-card-section>
+      </q-card>
+
+    </div>
+
+    <!-- Footer Section -->
+    <div class="footer-main-container">
+      <div class="floating-bar"></div>
+      <div class="footer-container">
+        <div class="footer-content-container">
+          <div class="footer-logo-container">
+            <img width="100px" src="../../public/gsl.png" alt="">
+          </div>
+          <p>this is it</p>
+          <p>this is it</p>
+        </div>
+        <div class="footer-content-container">
+
+        </div>
+        <div class="footer-content-container">
+
+        </div>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+
+import SvgIcon from '../components/SvgIcon.vue'
+export default {
+  name: 'IndexPage',
+
+  data() {
+    return {
+      text: ""
+    }
+  },
+
+  components: {
+    SvgIcon, // Register the SvgIcon component
+  }
+}
+
 </script>
+
+<!--  -->
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+}
+
+.header-parent {
+  display: block;
+  width: 100%;
+  height: 79svh;
+  position: relative;
+  overflow: hidden;
+}
+
+.header-parent::before {
+  display: block;
+  content: '';
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 73, 112, 0.922);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+.header-parent::after {
+  display: block;
+  content: '';
+  width: 20%;
+  height: 70%;
+  background: rgb(255, 255, 255);
+  position: absolute;
+  bottom: 0;
+  left: 10%;
+  border-radius: 100px 100px 0 0;
+  z-index: -1;
+}
+
+.hero-section-logos-contianer {
+  width: 20%;
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  position: absolute;
+  left: 10%;
+  bottom: 0;
+}
+
+.hero-section-logos-contianer>img:nth-child(1) {
+  width: 43%;
+}
+
+.hero-section-logos-contianer>img:nth-child(2) {
+  width: 40%;
+}
+
+.hero-section-logos-contianer>img:nth-child(3) {
+  width: 52%;
+}
+
+.header-parent>img {
+  width: 100%;
+  position: absolute;
+  top: -120px;
+  left: 0;
+  z-index: -2;
+}
+
+.grey-logo {
+  width: 20%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  justify-content: left;
+  padding: 0 0 0 3%;
+  align-items: center;
+}
+
+.grey-logo>img {
+  width: 55%;
+}
+
+.header-container {
+  display: flex;
+  width: 98%;
+  height: 12%;
+  padding: 10px 3%;
+  margin: auto;
+  justify-content: space-between;
+  background: white;
+  border-radius: 0 0 100px 100px;
+}
+
+.header-nav {
+  display: flex;
+  width: 45%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.header-nav>ul {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding: 0 20px;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  background: rgba(0, 73, 112, 0.074);
+  border-radius: 100px;
+}
+
+.header-nav>ul>li {
+  font-size: 1.3em;
+}
+
+.header-nav>ul>li>a {
+  color: rgb(0, 73, 112);
+  font-weight: 600;
+}
+
+/*  */
+
+.hero-section-main-contianer {
+  background: rgba(255, 255, 255, 0);
+  width: 100%;
+  height: 88%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.hero-section-contianer {
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+  justify-content: right;
+  align-items: flex-end;
+}
+
+.hero-section-contianer .button-container {
+  width: 70%;
+  height: 15%;
+  padding-left: 6%;
+  display: flex;
+  align-items: center;
+}
+
+.button-container>a {
+  display: flex;
+  margin-top: -50px;
+  background: rgb(255, 170, 0);
+
+  color: #fff;
+  font-size: 1.3em;
+  font-weight: 600;
+  text-decoration: none;
+  border-radius: 5px;
+  outline: 1px solid rgb(255, 170, 0);
+  outline-offset: 5px;
+  box-sizing: border-box;
+  height: 60px;
+  width: 180px;
+  justify-content: center;
+  align-items: center;
+  animation: outline .9s infinite ease-in-out;
+}
+
+.button-container>a>img {
+  width: 35px;
+  margin-top: 5px;
+  animation: arrow .9s infinite ease-in-out;
+}
+
+@keyframes outline {
+  0% {
+    outline-offset: 8px;
+  }
+
+  50% {
+    outline-offset: 5px;
+  }
+
+  100% {
+    outline-offset: 8px;
+  }
+}
+
+@keyframes arrow {
+  0% {
+    transform: translateX(0px)
+  }
+
+  50% {
+    transform: translateX(15px)
+  }
+
+  100% {
+    transform: translateX(0px)
+  }
+}
+
+.button-container>a:hover {
+  animation-name: none;
+}
+
+.hero-section-text-contianer {
+  width: 70%;
+  height: 85%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 6%;
+}
+
+.hero-section-text-contianer>h1 {
+  width: 100%;
+  font-size: 5em;
+  text-align: right;
+  font-family: Arial, sans-serif;
+  font-weight: 900;
+  color: rgb(255, 170, 0);
+}
+
+.hero-section-text-contianer>h1,
+.hero-section-text-contianer>p {
+  margin: 25px;
+}
+
+.hero-section-text-contianer>p {
+  width: 100%;
+  font-size: 2em;
+  font-family: Arial, sans-serif;
+  color: white;
+  text-align: left;
+}
+
+@media (min-width: 1700px) {
+  .header-parent {
+    width: 100%;
+    height: 87svh;
+  }
+
+  .hero-section-text-contianer>h1 {
+    width: 100%;
+    font-size: 6em;
+    text-align: right;
+    font-family: Arial, sans-serif;
+    font-weight: 900;
+    color: rgb(255, 170, 0);
+  }
+
+  .hero-section-text-contianer>p {
+    width: 100%;
+    font-size: 2.5em;
+    font-family: Arial, sans-serif;
+    color: white;
+    text-align: left;
+  }
+
+  .header-parent::after {
+    width: 17%;
+    height: 70%;
+    left: 13%;
+    border-radius: 100px 100px 0 0;
+  }
+
+  .hero-section-logos-contianer {
+    width: 17%;
+    height: 70%;
+    left: 13%;
+  }
+
+  .hero-section-logos-contianer>img:nth-child(1) {
+    width: 46%;
+  }
+
+  .hero-section-logos-contianer>img:nth-child(2) {
+    width: 40%;
+  }
+
+  .hero-section-logos-contianer>img:nth-child(3) {
+    width: 55%;
+  }
+
+  .hero-section-text-contianer {
+    padding: 6%;
+  }
+
+  .hero-section-text-contianer>h1,
+  .hero-section-text-contianer>p {
+    margin: 25px;
+  }
+
+  .hero-section-contianer .button-container {
+    width: 70%;
+    height: 15%;
+    padding-left: 6%;
+    display: flex;
+    align-items: center;
+  }
+
+  .button-container>a {
+    padding: 15px;
+    margin-top: -70px;
+    background: rgb(255, 170, 0);
+
+    color: #fff;
+    font-size: 1.3em;
+    font-weight: 600;
+    text-decoration: none;
+    border-radius: 5px;
+    outline: 1px solid rgb(255, 170, 0);
+    outline-offset: 5px;
+  }
+}
+
+/*  */
+
+.footer-main-container{
+  width: 100%;
+  height: 490px;
+  background: rgba(0, 73, 112, 0.074);
+  position: relative;
+  margin-top: 150px;
+}
+.footer-main-container .floating-bar{
+width: 75%;
+background: rgba(0, 73, 112);
+height: 130px;
+margin: auto;
+transform: translateY(-40px);
+box-shadow: -18px -18px  rgba(0, 73, 112, 0.201);
+}
+</style>
